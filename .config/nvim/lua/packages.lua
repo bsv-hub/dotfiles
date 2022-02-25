62f435461100250db754e9390f5c18f06c363453
bsv-hub <43187016+bsv-hub@users.noreply.github.com>
2022-02-25 12:03:10 +0300

    Update packages.lua

--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -87,17 +87,17 @@
 
 use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
-  use { 'nvim-treesitter/nvim-treesitter',
-        run = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = "maintained",
-                highlight = {
-                    enable = true,
-                }
-            }
-        end
-  }
+--   use { 'nvim-treesitter/nvim-treesitter',
+--         run = ":TSUpdate",
+--         config = function()
+--             require("nvim-treesitter.configs").setup {
+--                 ensure_installed = "maintained",
+--                 highlight = {
+--                     enable = true,
+--                 }
+--             }
+--         end
+--   }
 
   use {
     'hrsh7th/nvim-cmp',
